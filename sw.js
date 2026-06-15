c8657347fb7ac2d7af95d2f0b1bfb034efcc0c4c
Alexandre Nery <alpnery@gmail.com>
2026-06-15 14:42:20 +0000

    fix: localStorage backup layer + tx onabort + storage status indicator (v2.1)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'splitz-v2.0';
+const CACHE = 'splitz-v2.1';
 const CORE = ['./', 'logo.svg', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', e => {
